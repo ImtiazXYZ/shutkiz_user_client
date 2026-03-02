e5dd447d4a9b2fb00384ea318ad4c7cffb920649
refaduL <refadul.cse@gmail.com>
2026-03-02 19:23:58 +0600

    fix: resolve sitemap build collision

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.shutkiz.com", // 🔗 change this to your domain
-  generateRobotsTxt: true, // ✅ also creates robots.txt
-  sitemapSize: 7000, // optional: splits large sitemaps
-  exclude: ["/admin/*", "/api/*"], // optional: exclude routes
-};
+// currently using the Native Next.js App Router sitemap generation, so this file is not needed. Keeping it here for reference if we decide to switch to next-sitemap in the future.
+// "postbuild": "next-sitemap", is the script in package.json that triggers next-sitemap after the build process. If we decide to use next-sitemap, we will add this line.
+
+
+// /** @type {import('next-sitemap').IConfig} */
+// module.exports = {
+//   siteUrl: "https://www.shutkiz.com", // 🔗 change this to your domain
+//   generateRobotsTxt: true, // ✅ also creates robots.txt
+//   sitemapSize: 7000, // optional: splits large sitemaps
+//   exclude: ["/admin/*", "/api/*"], // optional: exclude routes
+// };
